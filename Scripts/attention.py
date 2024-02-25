47d2e1a9a0cfc36ec691457192c807fddb2b4fa7
Rizwan <rizowan69@gmail.com>
2024-02-25 23:53:29 +0600

    self attention and decoder

--- Scripts/attention.py
+++ Scripts/attention.py
@@ -4,3 +4,35 @@ from torch.nn import functional as F
 import math
 
 class SelfAttention(nn.Module):
+    def __init__(self, n_heads:int, d_embed:int, in_proj_bias=True,out_proj_bias=True,):
+        super().__init__()
+        self.in_proj = nn.Linear(d_embed, 3 * d_embed, bias=in_proj_bias)
+        self.out_proj = nn.Linear(d_embed, d_embed, bias=out_proj_bias)
+        self.n_head = n_heads
+        self.d_head = d_embed // n_heads
+
+    def forward(self, x: torch.Tensor, causal_mask=False):
+        input_shape = x.shape
+        batch_size, sequence_length, d_embed = input_shape
+        intermim_shape = (batch_size, sequence_length, self.n_head, self.d_head)
+
+        q, k, v = self.in_proj(x).chunk(3,dim=1)
+
+        q = q.view(intermim_shape).transpose(1, 2)
+        k = k.view(intermim_shape).transpose(1, 2)
+        v = v.view(intermim_shape).transpose(1, 2)
+
+        weight = q @ k.transpose(-1, -2)
+
+        if causal_mask:
+            mask = torch.ones_like(weight,dtype=torch.bool).triu_(1)
+            weight.masked_fill(mask, -torch.inf)
+
+        weight /= math.sqrt(self.d_head)
+        weight = F.softmax(weight, dim=1)
+        output = weight @ v
+        output = output.transpose(1,2)
+        output = output.reshape(input_shape)
+        output = self.out_proj(output)
+
+        return output
--- Scripts/decoder.py
+++ Scripts/decoder.py
@@ -43,3 +43,48 @@ class VAE_AttentionBlock(nn.module):
         x = x.view(n, c, h, w)
         x += residual
         return x
+
+class VAE_Decoder(nn.Sequential):
+    def __init__(self):
+        super().__init__(
+            nn.Conv2d(4, 4, kernel_size=1, padding=0),
+            nn.Conv2d(4,512,kernel_size=3, padding=1),
+            VAE_ResidualBlock(512,512),
+            VAE_AttentionBlock(512),
+            VAE_ResidualBlock(512, 512),
+            VAE_ResidualBlock(512, 512),
+            VAE_ResidualBlock(512, 512),
+            VAE_ResidualBlock(512, 512),
+
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(512,512,kernel_size=3,padding=1),
+
+            VAE_ResidualBlock(512, 512),
+            VAE_ResidualBlock(512, 512),
+            VAE_ResidualBlock(512, 512),
+
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+
+            VAE_ResidualBlock(512, 256),
+            VAE_ResidualBlock(256, 256),
+            VAE_ResidualBlock(256, 256),
+
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+
+            VAE_ResidualBlock(256, 128),
+            VAE_ResidualBlock(128, 128),
+            VAE_ResidualBlock(128, 128),
+
+            nn.GroupNorm(32,128),
+            nn.SiLU(),
+
+            nn.Conv2d(128, 3, kernel_size=3, padding=1)
+        )
+
+    def forward(self, x:torch.Tensor) -> torch.Tensor:
+        x /= 0.18215
+        for module in self:
+            x = module(x)
+        return x
